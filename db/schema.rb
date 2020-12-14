7b0df0a2946fd94c1a68077edce1bcd6617adc17
Spola1 <1111.22224@mail.ru>
2020-12-14 22:10:26 +0300

    lesson4

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_072721) do
+ActiveRecord::Schema.define(version: 2020_12_14_183956) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false
-    t.text "answer"
+    t.text "answer", null: false
     t.integer "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 2020_12_11_072721) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
+  end
+
+  create_table "test_passages", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "test_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["test_id"], name: "index_test_passages_on_test_id"
+    t.index ["user_id"], name: "index_test_passages_on_user_id"
   end
 
   create_table "tests", force: :cascade do |t|
