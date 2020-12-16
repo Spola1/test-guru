5eff7d9122464abe381447fff48b1a6f97206de7
Spola1 <1111.22224@mail.ru>
2020-12-16 20:46:24 +0300

    определение ассоциаций

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+  has_many :created_tests, class_name: "Test", foreign_key: :user_id, dependent: :nullify
+	has_many :test_passages
+	has_many :tests, through: :test_passages
+
   def tests_passage(level)
-  Test
+    Test
     .joins('JOIN test_passages ON tests.id = test_passages.test_id')
     .where(test_passages: {user_id: id})
     .where(level: level)
+  end
 end
