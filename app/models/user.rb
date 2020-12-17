53b520b19e484eef91e79a6175095b239a85701e
Spola1 <1111.22224@mail.ru>
2020-12-18 00:24:35 +0300

    fix_lesson5

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 	has_many :tests, through: :test_passages
 
   def tests_passage(level)
-    Test
+    tests.where(level: level)
     .joins('JOIN test_passages ON tests.id = test_passages.test_id')
     .where(test_passages: {user_id: id})
     .where(level: level)
