443d135a55b613073d4b7a7f9f474ccce7887f0e
Spola1 <1111.22224@mail.ru>
2020-12-18 00:26:23 +0300

    fix_lesson5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :created_tests, class_name: "Test", foreign_key: :user_id, dependent: :nullify
-	has_many :test_passages
-	has_many :tests, through: :test_passages
+  has_many :test_passages
+  has_many :tests, through: :test_passages
 
   def tests_passage(level)
     tests.where(level: level)
