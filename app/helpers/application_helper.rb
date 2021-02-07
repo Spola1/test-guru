59e463c4b7cdf49312d89f7cb4eca2a61d54fffc
Sola1 <1111.22224@mail.ru>
2021-02-07 12:32:10 +0300

    lesson13

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,19 @@
 module ApplicationHelper
-  
+
   def year
     Time.now.year
   end
 
   def github_author(author, repo)
     link_to 'TestGuru', "https://github.com/#{author}/#{repo}"
+  end
+
+  def flash_class(level)
+    case level
+      when 'notice' then "alert alert-info"
+      when 'success' then "alert alert-success"
+      when 'error' then "alert alert-danger"
+      when 'alert' then "alert alert-warning"
+    end
   end
 end
