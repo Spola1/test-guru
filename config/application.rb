41e5bdb128afb899946b56fc5e211dbde7a47ad3
Spola1 <1111.22224@mail.ru>
2021-03-01 01:16:33 +0300

    lesson17

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ Bundler.require(*Rails.groups)
 module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2    
-    config.time_zone = 'Ekaterinburg'
+    config.load_defaults 5.2
+    config.time_zone = 'Belarus/Minsk'
     config.i18n.default_locale = :ru
 
     # Settings in config/environments/* take precedence over those specified here.
