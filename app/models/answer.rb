4c33b5ad0432c48a803de86de3e92112b9de79e5
Spola1 <1111.22224@mail.ru>
2021-03-06 12:29:32 +0300

    lesson17

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,10 +4,10 @@ class Answer < ApplicationRecord
   scope :correct, -> { where(correct: true) }
 
   validates :body, presence: true
-  validate :validate_numbers_of_answers, on: :create
+  validate :validate_numbers_of_answers, on: :save
 
   def validate_numbers_of_answers
   	message = "кол-во ответов может быть не больше 4-х"
     errors.add(:question, message) if question.answers.count >= 4
-  end
+  end  
 end
--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,14 @@ module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.timezone = 'Belarus/Minsk'
-    config.i18n.default_locate :ru
+    config.time_zone = 'Minsk'
+    config.i18n.default_locale = :ru
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.autoload_paths << "#{Rails.root}/lib/clients"
+
   end
 end
