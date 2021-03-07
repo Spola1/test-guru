4d47c23aef61d8af4a2f9ce843ad5f55b60818a6
Spola1 <1111.22224@mail.ru>
2021-03-07 09:52:16 +0300

    lesson18timer_fix

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -29,7 +29,7 @@ class TestPassagesController < ApplicationController
   def update
     @test_passage.accept!(params[:answer_ids])
 
-    if @test_passage.completed? || @test_passage.test.duration && @test_passage.overtime?(@test_passage.end_time)
+    if @test_passage.completed? || (@test_passage.test.duration && @test_passage.overtime?(@test_passage.end_time))
       TestsMailer.completed_test(@test_passage).deliver_now
       redirect_to result_test_passage_path(@test_passage)
     else
